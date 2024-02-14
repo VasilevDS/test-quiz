2b5c03e3d3a4066af9d5924c39e6a899fa4af284
Vasilevds <dmitry.v@vezubr.ru>
2024-02-14 19:20:39 +0300

    Creating a controller for voting in a quiz

---
+++ src/Controller/Quiz/Vote/Handler.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Quiz\Vote;
+
+use App\DTO\Quiz\Request\VoteQuizRequest;
+use App\DTO\Quiz\Snapshot\QuizSnapshotData;
+use App\Exception\Api\ApiValidationRequestException;
+use App\Service\Query\QuizResultService;
+use App\Validator\Quiz\Vote\VoteQuizConstraint;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+final readonly class Handler
+{
+    public function __construct(
+        private ValidatorInterface $validator,
+        private QuizResultService $quizResultService,
+    ) {
+    }
+
+    public function handle(VoteQuizRequest $voteQuizRequest): QuizSnapshotData
+    {
+        $violationList = $this->validator->validate($voteQuizRequest, new VoteQuizConstraint());
+        if (0 !== $violationList->count()) {
+            throw new ApiValidationRequestException($violationList);
+        }
+
+        return $this->quizResultService->calculateAndSaveResult($voteQuizRequest);
+    }
+}
---
+++ src/Controller/Quiz/Vote/VoteController.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Quiz\Vote;
+
+use App\Controller\ApiController;
+use App\DTO\Quiz\Request\VoteQuizRequest;
+use App\Infrastructure\RequestMapper\MapRequestPayloadValueResolver;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class VoteController extends ApiController
+{
+    #[Route(path: '/api/quiz/vote', name: 'api_quiz_vote', methods: ['POST'])]
+    public function pass(
+        #[MapRequestPayload(resolver: MapRequestPayloadValueResolver::class)]
+        VoteQuizRequest $passQuizRequest,
+        Handler $handler,
+    ): JsonResponse {
+        $quizSnapshotData = $handler->handle($passQuizRequest);
+
+        return $this->json($quizSnapshotData);
+    }
+}
